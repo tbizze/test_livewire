ba652029a13137e1f453d560cebb11bd3c043610
Tiago Bizze <Tiago Bizze>
2024-04-17 21:45:05 -0300

    teste HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Documento;
 use App\Models\DocumentoTipo;
 use App\Models\PlanoCtaItem;
 use Illuminate\Http\Request;
@@ -10,7 +11,33 @@ class HomeController extends Controller
 {
     public function test()
     {
-        $test = $this->getRandom(DocumentoTipo::class);
+        $test = Documento::query()
+        ->with('hasDocumentoBaixa')
+        ->withAggregate('toPessoa', 'nome_razao')
+        ->withAggregate('toDocumentoStatus', 'nome')
+        ->withAggregate('toDocumentoClasse', 'nome')
+        ->withAggregate('toDocumentoTipo', 'nome')
+        ->withSum(['hasDocumentoItems'], 'valor')
+        ->withCount(['hasDocumentoItems'], 'id')
+        ->withCount(['hasDocumentoBaixa'], 'id')
+
+        ->get()
+        ;
+
+        $i=0;
+        $j = '';
+        foreach ($test as $item) {
+            if ($item->has_documento_baixa_count > 0) {
+                //$item['documento_status_id'] = 10;
+                //dd($item->documento_status_id);
+                //$item->update(['documento_status_id' => 1]);
+                $i++;
+                $j = $j . ', ' . $item->id;
+                //dd($item->id, 'test');
+            }
+        }
+
+        dd($i, $j, $test->toArray());
         /* $now = $this->faker->date();
 
         if ($data_emissao >= $now) {
