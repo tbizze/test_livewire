d154b3cf708bb6ad276f0bd249546ce5f11e610a
Tiago Bizze <Tiago Bizze>
2024-04-19 10:22:49 -0300

    alterado rotas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Livewire\Settings\PermissionIndex;
+use App\Livewire\Settings\RoleIndex;
+use App\Livewire\Settings\RolePermissionsEdit;
+use App\Livewire\Settings\UserRolesIndex;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +30,12 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+
+    Route::get('/setting/roles', RoleIndex::class)->name('setting.roles.index');
+    Route::get('/setting/permissions', PermissionIndex::class)->name('setting.permissions.index');
+    Route::get('/setting/role-permissions/{role}/edit', RolePermissionsEdit::class)->name('setting.role-permissions');
+    Route::get('/setting/user-has-roles', UserRolesIndex::class)->name('setting.user-roles.index');
 });
 
 
